fe90de09225e4fceda506ac81c5bb483a1ca2d1e
樺 <xd_301@yahoo.com.tw>
2023-03-13 16:49:48 +0800

    部屬

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,11 +1,27 @@
 import axios from 'axios';
 //共用網址資料的部分，存在baseUrl變數中
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
 //getTods GET瀏覽資料設定，使用非同步
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todos failed]', error);
   }
@@ -16,7 +32,7 @@ export const createTodo = async (payload) => {
   //<要新增todos物件裡面的title跟isDone (Key值)>
   const { title, isDone } = payload;
   try {
-    const res = await axios.post(`${baseUrl}/todos`, { title, isDone });
+    const res = await axiosInstance.post(`${baseUrl}/todos`, { title, isDone });
     return res.data;
   } catch (error) {
     console.error('[Create Todo failed]', error);
@@ -27,13 +43,16 @@ export const patchTodo = async (payload) => {
   //逗號後者的參數為<要更新todos物件裡面的id,title 跟isDone (Key值)>
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, { title, isDone });
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
+    });
     return res.data;
   } catch {}
 };
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data;
   } catch (error) {
     console.error('Delete Todo failed', error);
